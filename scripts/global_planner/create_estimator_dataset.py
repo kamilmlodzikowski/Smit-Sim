e53d0fdea769246cd3ea1be42f248338053ba391
Dangield <dan.gield@gmail.com>
2024-04-18 12:13:14 +0200

    Estimator creates multiple datasets when encountering too much data in order to combat memory leak during dataset saving.

--- scripts/global_planner/create_estimator_dataset.py
+++ scripts/global_planner/create_estimator_dataset.py
@@ -1,26 +1,35 @@
 #!/usr/bin/env python3
-from os import listdir
+from os import listdir, mkdir
 from tensorflow.data import Dataset, experimental
 from datetime import datetime
 import sys
+import math
+import random
 
-SHUFLE_SIZE = 30000000
+SHUFFLE_SIZE = 10000000
 
 def main(dirs):
 
+	# read and shuffle timeseries files' names
 	timeseries = [dirs[0] + f for f in listdir(dirs[0])]
 	for d in dirs[1:]:
 		timeseries = timeseries + [d + f for f in listdir(d)]
 	timeseries.sort()
+	random.shuffle(timeseries)
 
 	full_dataset = None
+	shard_i = 0
+	elements = 0
+	save_dir = 'estimator/datasets/' + datetime.now().strftime(f"%Y%m%d_%H%M%S_%f_F{len(timeseries)}_S{SHUFFLE_SIZE}")
 
+	# for each file add it to the dataset
 	for filename in timeseries:
-		print(f'Processing {filename}...')
+		print(f'Processing {filename}...', end = '\r')
 		file = open(filename, 'r')
 		contents = file.read().split('\n')
 		file.close()
 
+		# read file and restructure data
 		while contents[-1] == '':
 			contents = contents[:-1]
 		contents = [[float(value) for value in record.split(':')] for record in contents]
@@ -28,31 +37,44 @@ def main(dirs):
 		dataset = Dataset.from_tensor_slices(contents)
 		dataset = dataset.map(lambda window: (window[:-1], window[-1]))
 
+		# add data to dataset
 		if not full_dataset:
 			full_dataset = dataset
 		else:
 			full_dataset = full_dataset.concatenate(dataset)
 
-	# print(len(list(full_dataset)))
-	full_dataset = full_dataset.shuffle(SHUFLE_SIZE, reshuffle_each_iteration = False)
-	# for item in full_dataset:
-	# 	print(item)
-	# 	break
-	filename = 'estimator/datasets/' + datetime.now().strftime(f"%Y%m%d_%H%M%S_%f_F{len(timeseries)}_S{SHUFLE_SIZE}")
-	experimental.save(full_dataset, filename)
-	with open(filename + '.txt', 'w') as write_file:
+		# if dataset reaches set size save it and create another
+		if full_dataset.cardinality().numpy()/SHUFFLE_SIZE > 1.0:
+			print(f'Elements in shard: {full_dataset.cardinality()}')
+			elements += full_dataset.cardinality().numpy()
+			print(f'Shuffling shard {shard_i}.')
+			full_dataset.shuffle(SHUFFLE_SIZE, reshuffle_each_iteration = False)
+			print(f'Saving shard {shard_i}.')
+			experimental.save(full_dataset, save_dir + f'/{shard_i}')
+			shard_i += 1
+			full_dataset = None
+
+	# save dataset
+	if not (full_dataset is None):
+		print(f'Elements in shard: {full_dataset.cardinality()}')
+		elements += full_dataset.cardinality().numpy()
+		print(f'Shuffling shard {shard_i}.')
+		full_dataset.shuffle(SHUFFLE_SIZE, reshuffle_each_iteration = False)
+		print(f'Saving shard {shard_i}.')
+		experimental.save(full_dataset, save_dir + f'/{shard_i}')
+		shard_i += 1
+		full_dataset = None
+
+	# save dataset data
+	with open(save_dir + '/summary.txt', 'w') as write_file:
+		write_file.write(f'Elements in dataset: {elements}\n')
+		write_file.write(f'Number of shards: {shard_i}\n')
 		write_file.write('Directories:\n')
 		for d in dirs:
-			write_file.write(f'{d}\n')
+			write_file.write(f'\t{d}\n')
 		write_file.write('Timeseries:\n')
 		for t in timeseries:
-			write_file.write(f'{t}\n')
-
-
-	# new_dataset = experimental.load(filename)
-	# for item in new_dataset:
-	# 	print(item)
-	# 	break
+			write_file.write(f'\t{t}\n')
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
