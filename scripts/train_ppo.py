2c4a6b7ea7b03d903ca81d77d6bfa3c627b5da2e
Kamil Młodzikowski <k.mlodzikowski@gmail.com>
2025-09-03 12:37:31 +0200

    chore: update ray dependency

--- scripts/train_ppo.py
+++ scripts/train_ppo.py
@@ -2,9 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 import ray
 from ray import tune
-from ray.tune.logger import DEFAULT_LOGGERS
 from ray.tune.registry import register_env
-from ray.tune.integration.wandb import WandbLogger
 
 from smit_sim_env import SmitSimEnv
 
@@ -22,12 +20,10 @@ if __name__ == "__main__":
         "framework": "torch",
         "num_workers": 1,
         "model": {"fcnet_hiddens": [64, 64]},
-        "wandb": {"project": "smit-sim", "log_config": True},
     }
 
     tune.run(
         "PPO",
         config=config,
         stop={"training_iteration": 10},
-        loggers=DEFAULT_LOGGERS + (WandbLogger,),
     )
